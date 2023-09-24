43b49f532c810bd7f6e2e0823fb68ffba76a4c5b
thispeopledoesnotexist <thisemaildoesnotexist@thisemaildoesnotexist.com>
2023-09-24 06:26:00 -0300

    first angular project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { LoginComponent } from './login/login.component';
+import { ConsoleComponent } from './console/console.component';
+import { GamesComponent } from './games/games.component';
+import { PromotionComponent } from './promotion/promotion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'console', component:ConsoleComponent},
+  {path: 'games', component:GamesComponent},
+  {path: 'index', component: IndexComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'promotion', component: PromotionComponent},
+  {path: '', redirectTo: '/index', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,53 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+/*Angular Material Importation*/
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+/*Angular Material Importation*/
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IndexComponent } from './index/index.component';
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ConsoleComponent } from './console/console.component';
+import { GamesComponent } from './games/games.component';
+import { PromotionComponent } from './promotion/promotion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    LoginComponent,
+    FooterComponent,
+    ToolbarComponent,
+    ConsoleComponent,
+    GamesComponent,
+    PromotionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatMenuModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
